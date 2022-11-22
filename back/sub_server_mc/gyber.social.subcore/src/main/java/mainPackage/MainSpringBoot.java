1ced4a0e30fdb12e79e10529b011db4f2283c177
Nicki <casperplay45@gmail.com>
2022-11-22 17:14:45 +0200

    init branch commit

--- back/sub_server_mc/gyber.social.subcore/src/main/java/mainPackage/MainSpringBoot.java
+++ back/sub_server_mc/gyber.social.subcore/src/main/java/mainPackage/MainSpringBoot.java
@@ -1,10 +1,11 @@
 package mainPackage;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainSpringBoot {
     public static void main(String[] args) {
-        // ...
+        SpringApplication.run(MainSpringBoot.class, args);
     }
 }
--- back/sub_server_mc/gyber.social.subcore/src/main/java/tcpip/RequestObjectController.java
+++ back/sub_server_mc/gyber.social.subcore/src/main/java/tcpip/RequestObjectController.java
@@ -1,6 +1,7 @@
 package tcpip;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,7 +30,9 @@ public class RequestObjectController {
 
     @GetMapping("/auth")
     public ResponseEntity authUserAndReturnEntity(@RequestBody AuthDataUser authDataUser){
-        // ...
+       if(authDataUser == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+       }
         return ResponseEntity.ok().build();
     }
 
@@ -53,6 +56,8 @@ public class RequestObjectController {
         // ...
         return ResponseEntity.ok().build();
     }
+
+
 
 
 
