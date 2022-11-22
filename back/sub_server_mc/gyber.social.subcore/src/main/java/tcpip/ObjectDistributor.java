7f9b4ec715df183d15bf98660a1c99e7c05fe4fc
rozoomcool <rozoom.um@gmail.com>
2022-11-22 20:41:24 +0300

    add ObjectTransformer and ObjectDistributor

---
+++ back/sub_server_mc/gyber.social.subcore/src/main/java/tcpip/ObjectDistributor.java
@@ -0,0 +1,7 @@
+package tcpip;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ObjectDistributor {
+}
---
+++ back/sub_server_mc/gyber.social.subcore/src/main/java/tcpip/ObjectTransformer.java
@@ -0,0 +1,7 @@
+package tcpip;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ObjectTransformer {
+}
