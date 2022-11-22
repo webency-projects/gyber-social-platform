94b26c97e7f3c7f81ccdfaa7465ac48bc804d046
Nicki <casperplay45@gmail.com>
2022-11-22 17:03:23 +0200

    ...

--- back/sub_server_mc/gyber.social.subcore/src/main/java/mainPackage/MainSpringBoot.java
+++ back/sub_server_mc/gyber.social.subcore/src/main/java/mainPackage/MainSpringBoot.java
@@ -1,9 +1,6 @@
 package mainPackage;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class MainSpringBoot {
